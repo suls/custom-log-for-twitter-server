da540a21d45c9a4d7f07708672535c89daf760a2
Mathias Sulser <suls@suls.org>
2015-11-05 01:36:44 +0900

    proposed API change

--- build.sbt
+++ build.sbt
@@ -6,4 +6,8 @@ scalaVersion := "2.11.7"
 
 resolvers += "twttr" at "https://maven.twttr.com/"
 
-libraryDependencies += "com.twitter" %% "twitter-server" % "1.15.0"
+libraryDependencies ++=
+  Seq(
+    "com.twitter" %% "twitter-server" % "1.15.0"
+  , "com.twitter" %% "util-logging" % "6.30.0-SNAPSHOT"
+  )
--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -16,40 +16,10 @@ MyServer {
 trait MyLogging extends Logging {
   self: App =>
 
-  import Logging._
-
-  def myhandlers: List[() => Handler] = {
-    val output = outputFlag()
-    val level = Some(levelFlag())
-    val handler =
-      if (output == "/dev/stderr")
-        ConsoleHandler(level = level)
-      else
-        FileHandler(
-          output,
-          rollPolicyFlag(),
-          appendFlag(),
-          rotateCountFlag(),
-          level = level,
-          formatter = new Formatter(
-            timezone = Some("UTC")
-            ,prefix = "<yyyy-MM-dd HH:mm:ss.SSS> [%.3s] %s: "
-          )
-        )
-    //  if (asyncFlag())
-    //    handler = QueueingHandler(handler, asyncMaxSizeFlag())
-
-    handler :: Nil
-  }
-
-  override def loggerFactories: List[LoggerFactory] = {
-    LoggerFactory(
-      node = "",
-      level = Some(levelFlag()),
-      handlers = myhandlers
-    ) :: Nil
-  }
-
+  override def defaultFormatter = new Formatter(
+    timezone = Some("UTC")
+    , prefix = "<yyyy-MM-dd HH:mm:ss.SSS> [%.3s] %s: "
+  )
 }
 
 
